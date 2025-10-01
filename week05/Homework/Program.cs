f1d515c2b850501f8442f04a89c251f5721125ca
eperalta03 <eperaltagalaviz@byupatway.edu>
2025-10-01 17:35:49 -0600

    homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new MathAssignment("Juan", "Fractions", "7.1", "10-17");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Pedro", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
